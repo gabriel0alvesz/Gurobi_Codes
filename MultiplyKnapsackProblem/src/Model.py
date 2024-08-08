d9dd15b335060b643b3fc1b6d86370d29daab251
Gabriel Alves <gabriel0alves.oficial@gmail.com>
2024-08-08 18:08:29 +0000

    funcionamento ok

--- MultiplyKnapsackProblem/src/Model.py
+++ MultiplyKnapsackProblem/src/Model.py
@@ -1,53 +1,76 @@
 from ReadDocs import ReadDocs
 import gurobipy as gp
 from gurobipy import GRB
-import sys as s
+import sys
 
 class Model:
+    """Cria o modelo de otimização linear inteira para o problema das multiplas mochilas
+    """    
 
-    def __init__(self, args: str ):
+    def __init__(self):
         self.model = gp.Model()
     
     def create_decision_variables(self, n: int, m: int):
         self.x = self.model.addVars(n, m, vtype=GRB.BINARY, name='x')
         
     def define_objective(self, n: int, m: int, benefits: list):
-        sum = gp.quicksum(self.x[i,j] * benefits[i] for i in range(0,n) for j in range)
+        sum = gp.quicksum(self.x[i,j] * benefits[i] for i in range(0,n) for j in range(m))
         self.model.setObjective(sum, GRB.MAXIMIZE)
 
-    def constraints(self, n: int, m: int, weights: list):
+    def constraints(self, n: int, m: int, capacity:list,  weights: list):
         # Unicidade dos itens nas mochilas.
         for i in range(n):
             expr = gp.LinExpr()
             for j in range(m):
                 expr = expr + self.x[i, j]
-                self.model.addConstr(expr <= 1, name=f"item{i}")
+            self.model.addConstr(expr <= 1, name=f"item{i}")
             
         # Capacidade de cada mochila
         for j in range(m):
             expr = gp.LinExpr()
             for i in range(n):
                 expr = expr + (self.x[i, j] * weights[i])
-                self.model.addConstr(expr <= 1, name=f"backpack{j}")
+            self.model.addConstr(expr <= capacity[j], name=f"backpack{j}")
     
     def optimize_model(self):
         self.model.optimize()
     
-    def print_objetive_function(self, n: int, m: int):
-        # -> Como verificar se o modelo foi otimo?
-        print("---------------------------------------------------")
-        print("Solução Ótima")
-        print("---------------------------------------------------")
-        print(f"Valor da função objetivo: {self.model.ObjVal}")
-        
+    def print_result(self, n: int, m: int):
+        if(self.model.Status == GRB.OPTIMAL):
+            print("---------------------------------------------------")
+            print("Solução Ótima")
+            print("---------------------------------------------------")
+
+        print(f"Valor da Função Objetivo: {self.model.objVal}")  
         self.print_items(n, m)
 
-        
+
     def print_items(self, n: int, m: int):
         for j in range(m):
             items_bp = list()
             
             for i in range(n):    
-                if self.x[i,j].x == 1: items_bp.append(i+1)
+                if self.x[i,j].X == 1: items_bp.append(i+1)
             print(f"Itens na Mochila {j}: {items_bp}")
         
+
+if __name__ == "__main__":
+    
+    rd = ReadDocs()
+
+    try:
+        rd.read_Doc(sys.argv[1])
+    except:
+        print("---Erro ao abrir o arquivo!---")
+        exit()
+    
+    rd.tokenizer()
+    lines = rd.define_Variables()
+
+    model = Model()
+    model.create_decision_variables(n=lines[0], m=lines[1])
+    model.define_objective(n=lines[0], m=lines[1], benefits=lines[3])
+    model.constraints(n=lines[0], m=lines[1], capacity=lines[2], weights=lines[4])
+    model.optimize_model()
+
+    model.print_result(n=lines[0], m=lines[1])
--- MultiplyKnapsackProblem/src/ReadDocs.py
+++ MultiplyKnapsackProblem/src/ReadDocs.py
@@ -1,5 +1,12 @@
 
 class ReadDocs:
+    """Realiza a leitura do arquivo e retorna:\n
+        n = Quantidade de itens\n
+        m = Quantidade de mochilas.\n
+        b = Lista com a capacidade de cada mochila respectivamente.\n
+        p = Lista com os valores de benefício de cada item respectivamente.\n
+        w = Lista com os pesos de cada item respectivamente\n
+    """    
 
     def __init__(self):
         self.lines: list
@@ -12,24 +19,23 @@ class ReadDocs:
         self.lines = [line.strip().split() for line in self.lines]
 
     def define_Variables(self) -> list:
-        n, m = map(int, self.lines[0][0:2])
-        b = list(map(float, self.lines[1])) # capacidades de cada mochila
+        n, m = map(int, self.lines[0][0:2]) # n = Quantidade de itens & m = Numero de mochilhas
+        b = list(map(float, self.lines[1])) # Capacidades de cada mochila
         
-        p = list() # beneficios
-        w = list() # pesos
+        p = list() # Beneficios
+        w = list() # Pesos
         
         for e in range(0, n):
-            
-            p.append(self.lines[2+e][0])
-            w.append(self.lines[2+e][1])
+            p.append(float(self.lines[2+e][0]))
+            w.append(float(self.lines[2+e][1]))
 
-        return [n, m, p, w]
-       
+        return [n, m, b, p, w]
+
+# Para teste de leitura       
 if __name__ == "__main__":
     rd = ReadDocs()
 
     rd.read_Doc("input.txt")
-
     rd.tokenizer()
 
     print(rd.define_Variables())
