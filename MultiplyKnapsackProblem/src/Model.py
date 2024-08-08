4a111cf7932cbb182c373be24abcdca1ca31f953
Gabriel Alves <gabriel0alves.oficial@gmail.com>
2024-08-08 15:58:48 +0000

    prints ok
    falta verificar se a solução é otima

--- MultiplyKnapsackProblem/src/Model.py
+++ MultiplyKnapsackProblem/src/Model.py
@@ -12,14 +12,42 @@ class Model:
         self.x = self.model.addVars(n, m, vtype=GRB.BINARY, name='x')
         
     def define_objective(self, n: int, m: int, benefits: list):
-        sum = gp.quicksum(benefits[i] * self.x[i,j] for i in range(0,n) for j in range)
+        sum = gp.quicksum(self.x[i,j] * benefits[i] for i in range(0,n) for j in range)
         self.model.setObjective(sum, GRB.MAXIMIZE)
 
-    def constraints(self, n: int, m: int):
-        # Unanidade dos intens na mochila. -> teste
+    def constraints(self, n: int, m: int, weights: list):
+        # Unicidade dos itens nas mochilas.
         for i in range(n):
             expr = gp.LinExpr()
             for j in range(m):
                 expr = expr + self.x[i, j]
-                self.model.addConstr(expr <= 1, name=f"item_{i}")
+                self.model.addConstr(expr <= 1, name=f"item{i}")
+            
+        # Capacidade de cada mochila
+        for j in range(m):
+            expr = gp.LinExpr()
+            for i in range(n):
+                expr = expr + (self.x[i, j] * weights[i])
+                self.model.addConstr(expr <= 1, name=f"backpack{j}")
+    
+    def optimize_model(self):
+        self.model.optimize()
+    
+    def print_objetive_function(self, n: int, m: int):
+        # -> Como verificar se o modelo foi otimo?
+        print("---------------------------------------------------")
+        print("Solução Ótima")
+        print("---------------------------------------------------")
+        print(f"Valor da função objetivo: {self.model.ObjVal}")
+        
+        self.print_items(n, m)
 
+        
+    def print_items(self, n: int, m: int):
+        for j in range(m):
+            items_bp = list()
+            
+            for i in range(n):    
+                if self.x[i,j].x == 1: items_bp.append(i+1)
+            print(f"Itens na Mochila {j}: {items_bp}")
+        
