020e5acca2f7b834362cc21884e1baa922171081
Gabriel Alves <gabriel0alves.oficial@gmail.com>
2024-08-08 14:13:39 +0000

    first commit and init knaspsack problem

---
+++ MultiplyKnapsackProblem/src/ReadDocs.py
@@ -0,0 +1,43 @@
+
+class ReadDocs:
+
+    def __init__(self):
+        self.lines: list
+    
+    def read_Doc(self, name_doc: str):
+        with open(name_doc,'r') as f:
+            self.lines = f.readlines()
+    
+    def tokenizer(self):
+        self.lines = [line.strip().split() for line in self.lines]
+
+    # 5 2
+    # 9 6
+    # 5 3
+    # 10 2
+    # 7 9
+    # 6 2
+    # 6 10
+
+    def define_Variables(self) -> list:
+        n, m = map(int, self.lines[0][0:2])
+        b = list(map(float, self.lines[1])) # capacidades de cada mochila
+        
+        p = list() # beneficios
+        w = list() # pesos
+        
+        for e in range(0, n):
+            
+            p.append(self.lines[2+e][0])
+            w.append(self.lines[2+e][1])
+
+        return [n, m, p, w]
+       
+if __name__ == "__main__":
+    rd = ReadDocs()
+
+    rd.read_Doc("input.txt")
+
+    rd.tokenizer()
+
+    print(rd.define_Variables())
